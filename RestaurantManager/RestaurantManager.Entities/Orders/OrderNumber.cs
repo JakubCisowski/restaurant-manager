a359adeeb733f2e60ab01dfb99ac8fbaf20b2acb
Pawel Plowucha [ITEO] <pawel.plowucha@iteo.com>
2021-08-13 14:57:07 +0200

    init generator

---
+++ RestaurantManager/RestaurantManager.Entities/Orders/OrderNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Entities.Orders
+{
+    //seed new 5 000 000 records
+    public class OrderNumber
+    {
+        public int Id { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime InUsageFrom { get; set; }
+        // older than 2 months
+    }
+}
---
+++ RestaurantManager/RestaurantManager.Services/Services/OrderServices/OrderNoGeneratorService.cs
@@ -0,0 +1,32 @@
+﻿using RestaurantManager.Infrastructure.UnitOfWork;
+using RestaurantManager.Services.Services.OrderServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Services.Services.OrderServices
+{
+    public class OrderNoGeneratorService : IOrderNoGeneratorService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderNoGeneratorService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public int GenerateOrderNo()
+        {
+            Random rand = new Random();
+            //1. sprawdzasz czy w tabeli OrderNumber instnieje id rand
+            //2. Jeśli nie istnieje to tworzysz rekord o tym id
+            //3. Jeśli istnieje to sprawdzasz czy dostępny
+            //4. Jeśli nie to sprawdzasz czy można na odblokować
+            //5. Jeśli nie to następny wolny
+            // zapytać dominika co robić z przedawnionymi idkami
+            return 0;
+        }
+    }
+}
