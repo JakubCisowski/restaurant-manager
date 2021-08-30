6a1c26056f7042e31228c568a6003d11630fe320
Pawel Plowucha [ITEO] <pawel.plowucha@iteo.com>
2021-08-30 15:08:31 +0200

    init geocoding api

---
+++ RestaurantManager/RestaurantManager.Core/Integration/GeocodingApi.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Options;
+using RestaurantManager.Core.Integration.Model;
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Core.Integration
+{
+    public interface IGeocodingApi
+    {
+
+    }
+    public class GeocodingApi : IGeocodingApi
+    {
+        private readonly RestClient _client;
+
+        public GeocodingApi(IOptions<object> geocodingConfig)
+        {
+            //config
+
+            _client = new RestClient();//api url
+            //client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+        }
+
+        public GeocodingApiResponse GetCordinatesFromAdress(string adress)
+        {
+
+            return new GeocodingApiResponse();
+        }
+
+    }
+}
---
+++ RestaurantManager/RestaurantManager.Core/Integration/Model/GeocodingApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Core.Integration.Model
+{
+    public class GeocodingApiResponse
+    {
+        public List<decimal> Cordinates { get; set; }
+    }
+}
