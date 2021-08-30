113f2e73f23ca1cbbdc0efd64c04f5a63a0bfb32
Jakub Cisowski <jakub.cisowski@iteo.com>
2021-08-30 15:12:32 +0200

    Revert previous http request system.

--- RestaurantManager/RestaurantManager.Api/Program.cs
+++ RestaurantManager/RestaurantManager.Api/Program.cs
@@ -1,9 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using System;
-using System.IO;
-using System.Net;
 
 namespace RestaurantManager.Api
 {
@@ -12,22 +9,6 @@ namespace RestaurantManager.Api
 
         public static void Main(string[] args)
         {
-            string html = string.Empty;
-            string url = @"https://address.search.api.ongeo.pl/1.0/autocomplete?api_key=xd-bf09-46f4-8ec3-8cea2de3b9c1&query=krakow+wadowicka+8a";
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.AutomaticDecompression = DecompressionMethods.GZip;
-
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            using (Stream stream = response.GetResponseStream())
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                html = reader.ReadToEnd();
-            }
-
-            Console.WriteLine(html);
-
-
             CreateHostBuilder(args).Build().Run();
         }
 
