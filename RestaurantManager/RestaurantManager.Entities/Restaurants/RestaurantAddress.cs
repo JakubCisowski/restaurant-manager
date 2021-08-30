426b5a7c88fb4370ec1cac5f5dc9537bcff4b448
Jakub Cisowski <jakub.cisowski@iteo.com>
2021-08-30 08:49:48 +0200

    Add private sets in restaurant address entity.

--- RestaurantManager/RestaurantManager.Entities/Restaurants/RestaurantAddress.cs
+++ RestaurantManager/RestaurantManager.Entities/Restaurants/RestaurantAddress.cs
@@ -19,15 +19,15 @@ namespace RestaurantManager.Entities.Restaurants
             RestaurantId = restaurantId;
         }
 
-        public string Country { get; set; }
-        public string City { get; set; }
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public string ZipPostalCode { get; set; }
-        public string PhoneNumber { get;  set; }
+        public string Country { get; private set; }
+        public string City { get; private set; }
+        public string Address1 { get; private set; }
+        public string Address2 { get; private set; }
+        public string ZipPostalCode { get; private set; }
+        public string PhoneNumber { get; private set; }
 
         public Guid RestaurantId { get; private set; }
-        private Restaurant Restaurant { get; set; }
+        public  Restaurant Restaurant { get; private set; }
 
     }
 }
